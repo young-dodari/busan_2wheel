8f2fcd94887c6b430b43dca9e4177025ba9eeeb5
121click <projectvanced@gmail.com>
2022-08-10 18:29:37 +0900

    1

--- crawling.py
+++ crawling.py
@@ -5,6 +5,7 @@ import datetime
 
 serviceKey = 'vTfdXtGsny7xRerwunqDq1PezLyeNmoFmRIx2YwnlYV4sA8EZfBl%2BKsVcjjugqAkuFX1KCdo4lag3L91sHFX%2BQ%3D%3D'
 
+# code 1
 def getRequestUrl(url):
     '''
     URL 접속 요청 후 응답함수
@@ -23,17 +24,16 @@ def getRequestUrl(url):
         print(f"[{datetime.datetime.now()}] Error for URL : {url}")
         return None
 
-
+# code 2
 def getInterSectionInfo():
     service_url = 'http://apis.data.go.kr/6260000/CrossCartypeTrafficeVolumeService/getCrossCartypeTrafficeVolumeList'
-    params = f'?serviceKey={serviceKey}' # 인증키
-    params += f'&numOfRows=10'
+    params = f'?serviceKey={serviceKey}&numOfRows=200&resultType=json&CLCT_DT=201809051205' # 인증키
+
     params += f'&pageNo=1'
-    params += f'&resultType=json'
-    params += f'&CLCT_DT=201809051205'
+
     url = service_url + params
     
-    retData = getRequestUrl(url)
+    retData = getRequestUrl(url)        #code 1
 
     if retData == None:
         return None
@@ -41,7 +41,8 @@ def getInterSectionInfo():
         return json.loads(retData)
 
 
-def getInterSectionService():
+# code 3
+def getInterSectionService(item, result):
     result = []
 
     jsonData = getInterSectionInfo()
@@ -54,29 +55,35 @@ def getInterSectionService():
             for item in jsonData['getCrossCartypeTrafficVolumeList']['item']:
                 ISTL_LCTN = item['ISTL_LCTN']
                 CLCT_DT = item['CLCT_DT']
-                SUM_LRGTFVL = item['SUM_LRGTFVL']
-                Y_CRDN = item['Y_CRDN']
-                IXR_ID = item['IXR_ID']
                 IXR_NM = item['IXR_NM']
+                SUM_LRGTFVL = item['SUM_LRGTFVL']
                 SUM_MDDLTFVL = item['SUM_MDDLTFVL']
                 SUM_SMALTFVL = item['SUM_SMALTFVL']
                 X_CRDN = item['X_CRDN']
-                CMRA_ID = item['CMRA_ID']
+                Y_CRDN = item['Y_CRDN']
                 
-                result.append([ISTL_LCTN,CLCT_DT,SUM_LRGTFVL,Y_CRDN,IXR_ID,IXR_NM,SUM_MDDLTFVL,SUM_SMALTFVL,X_CRDN,CMRA_ID])
+                result.append({'방면':ISTL_LCTN,'수집일시':CLCT_DT,'교차로명':IXR_NM,'대형':SUM_LRGTFVL,'중형':SUM_MDDLTFVL,'소형':SUM_SMALTFVL,'경도':X_CRDN,'위도':Y_CRDN})
 
-    return result
-
-
+    # return result
 
 
+
+
+# code 0
 def main():
+    result=[]
+    # jsonResult=[]
+    jsonRes = getInterSectionInfo()         #code 2
 
-    jsondata = getInterSectionService()
 
-    print(jsondata)
+    # print(jsondata)
+    for item in jsonRes['item']:
+        getInterSectionService(item, result)        #code 3
 
 
+    with open(f'./교차로통행량정보테스트.json', mode='w', encoding='utf-8') as outfile:
+        jsonFile = json.dumps(result, indent=4, sort_keys=True, ensure_ascii=False)
+        outfile.write(jsonFile)
 
 
 if __name__ == '__main__':
