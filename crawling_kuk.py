3242395cc33d097a4c393d12215e0275ce853e1c
121click <projectvanced@gmail.com>
2022-08-10 20:30:56 +0900

    kuk

---
+++ crawling_kuk.py
@@ -0,0 +1,93 @@
+import urllib.request
+import json
+import pandas as pd
+import datetime
+
+serviceKey = 'vTfdXtGsny7xRerwunqDq1PezLyeNmoFmRIx2YwnlYV4sA8EZfBl%2BKsVcjjugqAkuFX1KCdo4lag3L91sHFX%2BQ%3D%3D'
+
+# code 1
+def getRequestUrl(url):
+    '''
+    URL 접속 요청 후 응답함수
+    ---------------------------------
+    parameter : url -> OPENAPI 전체 URL
+    '''
+    req = urllib.request.Request(url)
+
+    try:
+        res = urllib.request.urlopen(req)
+        if res.getcode() == 200:
+            print(f"[{datetime.datetime.now()} ] Url Request Success")
+            return res.read().decode('utf-8')
+    except Exception as e:
+        print(e)
+        print(f"[{datetime.datetime.now()}] Error for URL : {url}")
+        return None
+
+# code 2
+def getInterSectionInfo(cnt):
+    service_url = 'http://apis.data.go.kr/6260000/CrossCartypeTrafficeVolumeService/getCrossCartypeTrafficeVolumeList'
+    params = f'?serviceKey={serviceKey}&numOfRows=200&resultType=json&CLCT_DT=201809051205' # 인증키
+
+    params += f'&pageNo={cnt}'
+
+    url = service_url + params
+    
+    retData = getRequestUrl(url)        #code 1
+
+    if retData == None:
+        return None
+    else:
+        return json.loads(retData)
+
+# def getInterSectionInfo():
+#     service_url = 'http://apis.data.go.kr/6260000/CrossCartypeTrafficeVolumeService/getCrossCartypeTrafficeVolumeList'
+#     params = f'?serviceKey={serviceKey}&numOfRows=200&resultType=json&CLCT_DT=201809051205' # 인증키
+
+#     params += f'&pageNo=1'
+
+#     url = service_url + params
+    
+#     retData = getRequestUrl(url)        #code 1
+
+#     if retData == None:
+#         return None
+#     else:
+#         return json.loads(retData)
+
+def main():
+    cnt = 0
+    result = []
+    jsonData = getInterSectionInfo(cnt) #code 2
+    
+    if jsonData['getCrossCartypeTrafficVolumeList']['header']['code'] == '00':
+        if jsonData['getCrossCartypeTrafficVolumeList']['item'] == '':
+            print('서비스 오류!!')
+        else:
+            
+            while jsonData is None:
+                cnt += 1
+                jsonData = getInterSectionInfo(cnt)
+                result.append(jsonData['getCrossCartypeTrafficVolumeList']['item'])
+
+                for item in jsonData['getCrossCartypeTrafficVolumeList']['item']:
+                    ISTL_LCTN = item['ISTL_LCTN']
+                    CLCT_DT = item['CLCT_DT']
+                    IXR_NM = item['IXR_NM']
+                    SUM_LRGTFVL = item['SUM_LRGTFVL']
+                    SUM_MDDLTFVL = item['SUM_MDDLTFVL']
+                    SUM_SMALTFVL = item['SUM_SMALTFVL']
+                    result.append([ISTL_LCTN, CLCT_DT, IXR_NM, SUM_LRGTFVL, SUM_MDDLTFVL, SUM_SMALTFVL])
+                
+            df = pd.DataFrame(result, columns=['ISTL_LCTN', 'CLCT_DT', 'IXR_NM', 'SUM_LRGTFVL', 'SUM_MDDLTFVL', 'SUM_SMALTFVL'])
+            df.to_csv('intersection.csv', index=False, encoding='utf-8')
+            print('intersection.csv 파일 생성 완료')
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
